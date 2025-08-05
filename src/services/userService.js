e395cf9aedefb3dd874842689fadc8a257ea6d7a
Thuong <phamhoaithuong7008@gmail.com>
2025-08-05 16:39:42 +0700

    api user

---
+++ src/services/userService.js
@@ -0,0 +1,66 @@
+import { where } from "sequelize";
+import db from "../models/index";
+import bcrypt from "bcryptjs/dist/bcrypt"
+import { hash } from "bcrypt";
+import { raw } from "body-parser";
+
+
+let handleUserLogin = async (email, password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let userData = {};
+            let isExist = await checkUserEmail(email);
+            if (isExist) {
+                let user = await db.User.findOne({
+                    attributes: ['email', 'roleId', 'password'],
+                    where: { email: email },
+                    raw: true,
+
+                });
+                if (user) {
+                    let check = await bcrypt.compareSync(password, user.password);
+                    if (check) {
+                        userData.errCode = 0;
+                        userData.errMessage = 'OK';
+                        delete user.password;
+                        userData.user = user;
+                    } else {
+                        userData.errCode = 3;
+                        userData.errMessage = 'Wrong password';
+                    }
+                } else {
+                    userData.errCode = 2;
+                    userData.errMessage = `User is not found`;
+                }
+            } else {
+                userData.errCode = 1;
+                userData.errMessage = `Email not found`;
+            }
+            resolve(userData);
+
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+let checkUserEmail = (userEmail) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { email: userEmail }
+            })
+            if (user) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+module.exports = {
+    handleUserLogin: handleUserLogin,
+};
